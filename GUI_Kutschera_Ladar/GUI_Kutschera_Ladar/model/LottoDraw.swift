b148b169e7eabd997e3d55cb73a0b54ee38011e9
edi <philippeduard.ladar@edu.fh-joanneum.at>
2018-11-24 15:48:24 +0100

    fixed some code duplications

--- GUI_Kutschera_Ladar/GUI_Kutschera_Ladar/model/LottoDraw.swift
+++ GUI_Kutschera_Ladar/GUI_Kutschera_Ladar/model/LottoDraw.swift
@@ -21,13 +21,26 @@ func convertCsvAndSaveToDB(csv: String) {
   let entity = NSEntityDescription.entity(forEntityName: "LottoDrawEntity", in: context)
   let lottoDraw = NSManagedObject(entity: entity!, insertInto: context)
   let parsedLottoDraw = csv.parseCsv()
-  lottoDraw.setValue(Int16(parsedLottoDraw[0])!, forKey: "number1")
-  lottoDraw.setValue(Int16(parsedLottoDraw[1])!, forKey: "number2")
-  lottoDraw.setValue(Int16(parsedLottoDraw[2])!, forKey: "number3")
-  lottoDraw.setValue(Int16(parsedLottoDraw[3])!, forKey: "number4")
-  lottoDraw.setValue(Int16(parsedLottoDraw[4])!, forKey: "number5")
-  lottoDraw.setValue(Int16(parsedLottoDraw[5])!, forKey: "number6")
-  lottoDraw.setValue(Int16(parsedLottoDraw[0])!, forKey: "numberZz")
+  do {
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[0]), forKey: "number1")
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[1]), forKey: "number2")
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[2]), forKey: "number3")
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[3]), forKey: "number4")
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[4]), forKey: "number5")
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[5]), forKey: "number6")
+   try lottoDraw.setValue(convertStringToLottoNumber(parsedLottoDraw[0]), forKey: "numberZz")
+  } catch LottoDrawException.invalidArguments {
+    print("Cannot convert the lotto draw number. Something went wrong!")
+  } catch {
+    print("Unknown Error")
+  }
 }
 
 // TODO: guard function for lottodraw inputs
+func convertStringToLottoNumber(_ numberAsString: String) throws -> Int16 {
+  let number = Int16(numberAsString) ?? 0
+  guard number <= 45 && number >= 1 else {
+    throw LottoDrawException.invalidArguments
+  }
+  return number
+}
